202cd498dc921034714dfca01202de0eb843886a
Spencer Scott <spencerscott917@gmail.com>
2024-08-23 14:45:45 -0600

    adjust pagesize to mac's actual page size

--- godb/BTree.go
+++ godb/BTree.go
@@ -1,9 +1,9 @@
 package main
 
 const HEADER = 4
-const BTREE_PAGE_SIZE = 4096
+const BTREE_PAGE_SIZE = 16384
 const BTREE_MAX_KEY_SIZE = 1000
-const BTREE_MAX_VAL_SIZE = 3000
+const BTREE_MAX_VAL_SIZE = 15000
 
 func init() {
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
