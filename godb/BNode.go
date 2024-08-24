68f496aadb045e5dee6baeba2b6d9d8e262b82d3
Spencer Scott <spencerscott917@gmail.com>
2024-08-24 08:38:57 -0600

    add getPointer func

--- godb/BNode.go
+++ godb/BNode.go
@@ -21,3 +21,11 @@ func (node BNode) setHeader(nodeType uint16, nKeys uint16) {
 	binary.LittleEndian.PutUint16(node[0:2], nodeType)
 	binary.LittleEndian.PutUint16(node[2:4], nKeys)
 }
+
+func (node BNode) getPointer(idx uint16) uint16 {
+	if idx >= node.nKeys() {
+		panic("Attempted to get pointer outside of node")
+	}
+	pos := HEADER_SIZE + 8*idx
+	return binary.LittleEndian.Uint16(node[pos:])
+}
